8f4dfa811c19266d996930285d79454bb49369f2
BatM <maximiliano.brolio@fing.edu.uy>
2024-10-14 08:49:58 -0300

    Create foros.js

---
+++ foros.js
@@ -0,0 +1,22 @@
+const axios=require('axios')
+const cheerio=require('cheerio')
+const readline = require('readline')
+async function getForumLastReplies(forum_id,limit){
+    const url=`https://eva.fing.edu.uy/mod/forum/view.php?id=${forum_id}`
+    try{
+        const r=await axios.get(url)
+    }catch{
+     return [] 
+    }
+    const $=cheerio.load(r.data);
+    const publicaciones=$('th').map(function(){
+        let publicacion=$(this).find('a')
+        return {
+              titulo:publicacion.attr('title'),
+              url:publicacion.attr('href')  
+        }
+    }).toArray().filter(e=>e.titulo)
+    
+    return limit?publicaciones.slice(0,limit+1):publicaciones;
+}
+module.exports=getForumLastReplies
