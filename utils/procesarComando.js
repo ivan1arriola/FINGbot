825e2e0ebf497eef74c010063b0ea5c2f28c65e7
BatM <maximiliano.brolio@fing.edu.uy>
2024-10-16 00:07:47 -0300

    Cambiado para que detecte parametros entre ""

--- utils/procesarComando.js
+++ utils/procesarComando.js
@@ -6,23 +6,30 @@ const esComandoValido = (command, commandMap) => {
     return command in commandMap && typeof commandMap[command].func === 'function';
 };
 
+
 // Funciones auxiliares
-function obtenerArgumentos(body, commandObj) {
-    return body.trim().split(" ").slice(1).filter(text => text !== '').join(" ").split(" ", commandObj.args_size);
+function parseCommand(input) {
+    const regex = /(?:[^\s"]+|"[^"]*")+/g; // Detecta argumentos con espacios, los argumentos entre "" swran tratados como uno solo
+    const parts = input.match(regex).map(part => part.replace(/"/g, ''));
+    const comando = parts.shift().toLowerCase();
+    const args = parts;
+    return [
+        comando,
+        args
+    ];
 }
 
 // Función para procesar el comando
 const procesarComando = async (client, message, commandMap) => {
-    const [command] = message.body.trim().toLowerCase().slice(config.PREFIJO.length).split(" ");
-
+    const [command,args] = parseCommand(message.body.trim().slice(config.PREFIJO.length));
+    
     // Verificar si el comando es válido usando la nueva función
     if (esComandoValido(command, commandMap)) {
         // Imprimir por consola el comando válido con colores usando secuencias de escape ANSI
         console.log(`\x1b[32m✔ Se detectó comando válido: \x1b[34m[${command}]\x1b[0m`);
 
         const commandObj = commandMap[command];
-        const args = obtenerArgumentos(message.body, commandObj);
-
+        
         // Verificar el mínimo de argumentos
         if (args.length < commandObj.min_args) {
             return await message.reply(usage(commandObj));
