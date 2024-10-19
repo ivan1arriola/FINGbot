4ec75cef0f8fa423cbb00dc849f640066e388558
BatM <maximiliano.brolio@fing.edu.uy>
2024-10-19 20:53:16 -0300

    eliminado basura y cambiado la estructura

--- modulos/PokeGuesser.js
+++ modulos/PokeGuesser.js
@@ -2,11 +2,6 @@ const axios=require('axios')
 const { MessageMedia } = require('whatsapp-web.js')
 const fs=require('fs')
 const fsAsync = fs.promises
-const readline = require('readline')
-const rl = readline.createInterface({
-    input:process.stdin,
-    output:process.stdout
-    })
 if(fs.existsSync('pokemons.json')){
     var inventory=JSON.parse(fs.readFileSync('pokemons.json','utf-8'))
 }else{
@@ -17,51 +12,50 @@ let template={
     name:'Test',
     alias:[],
     winner:'Ganador',
-    timer:null
+    ev:null
 }
 
 let inventory_player={
     pokemons:[]
 }
+async function getChatId(message){
+    let chat=await message.getChat()
+    return chat.id._serialized
+    
+}
 async function save(){
     fsAsync.writeFile('pokemons.json',JSON.stringify(inventory))
+}
+async function addPlayerPokemon(author,pokemon){
+    let store={pokemones:inventory[author]?inventory[author].pokemones:{}}
+    store.pokemones[pokemon.name]=store.pokemones[pokemon.name]?store.pokemones[pokemon.name]+1:1
+    inventory[autor]=store
 }
 function randInt(max){
     return Math.floor(Math.random()*max)
 }
-function delay(ms,func){
-    return new Promise(resolve=>setTimeout(resolve,ms,val));
 
-}
-function question(text){
-    return new Promise(resolve=>rl.question(text,resolve));
-}
 async function stopGame(client,message){
     await message.reply('Juego terminado')
-    if(message.fromMe){
-       let chat=await message.getChat()
-       let chatid=chat.id._serialized?chat.id._serialized:chat.id
-    }else{
-       let chatid=message.from
-    }
+    let chatid=getChatId(message)
     if(chats[chatid]){
-        delete chats[chat.id._serialized]
+        delete chats[chatid]
     }
 }
+async function addGame(client,message,chatid,name,alias){
+    chats[chatid]=template
+    chats[chatid].alias=alias
+    chats[chatid].name=pokemon_name
+    chats[chatid].winner=''
+    chats[chatid].ev=setTimeout(stopGame,1000*300,client,message)
+}
 async function createGame(client,message,args){
-
-    if(message.fromMe){
-       let chat=await message.getChat()
-       let chatid=chat.id._serialized?chat.id._serialized:chat.id
-    }else{
-       let chatid=message.from
-    }
+    let chatid=getChatId(message)
     if(chatid in chats && chats[chatid].winner=='') return await message.reply('Ya hay un juego en curso') 
     let pokemon_count=(await axios.get('https://pokeapi.co/api/v2/pokemon?limit=1')).data.count
     let pokemon_picked=randInt(pokemon_count)
     let resp=await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${pokemon_picked}`)
     let pokemon_dict=(resp.data.results[0])
-
     let pokemon_name=pokemon_dict.name
     let pokemon_id=pokemon_dict.url.trim().split('/',7).pop()
     let pokemon_image=`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon_id}.png`.trim()
@@ -69,22 +63,13 @@ async function createGame(client,message,args){
     pokemon_name.split('-').forEach(a=>alias.push(a))
     pokemon_name.split(' ').forEach(a=>alias.push(a))
     alias.push(pokemon_name.replace('-',' '))
-    chats[chatid]=template
-    chats[chatid].alias=alias
-    chats[chatid].name=pokemon_name
-    chats[chatid].winner=''
-    chats[chatid].ev=setTimeout(stopGame,1000*300,client,message)
+    await addGame(client,message,pokemon_name,alias)
     let media=await  MessageMedia.fromUrl(pokemon_image)
     const msg=`Adivina el pokemon con !guess <nombre>`
     await message.reply(msg,null,{media:media})
 }
 async function tryGuess(client,message,args){
-   if(message.fromMe){
-       let chat=await message.getChat()
-       let chatid=chat.id._serialized?chat.id._serialized:chat.id
-   }else{
-       let chatid=message.from
-   }
+   let chatid=getChatId(message)
    let cid=chats[chatid]
    let autor=message.author
    if(!cid) return await message.reply('No hay ninguna partida en curso')
@@ -96,9 +81,7 @@ async function tryGuess(client,message,args){
        if(cid.ev){
            clearTimeout(cid.ev);
            chats[chatid].ev=null;
-           let store={pokemones:inventory[autor]?inventory[autor].pokemones:{}}
-           store.pokemones[cid.name]=store.pokemones[cid.name]?store.pokemones[cid.name]+1:1
-           inventory[autor]=store
+           await addPlayerPokemon(autor,cid)
 
        }
        cid.winner=autor
