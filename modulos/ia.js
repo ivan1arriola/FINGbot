df42a237f73a8064989c06e4e29311e3e4fff1d3
BatM <BatM@FingBot.com>
2024-10-22 13:56:56 -0300

    Arreglado modulo ia

--- modulos/ia.js
+++ modulos/ia.js
@@ -1,26 +1,38 @@
 const model="DANGODxH0b934"
+const customPrompt=`
+Eres un estudiante de ingenieria
+
+
+`
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
-function buildResponse(message,useModel,model){
-        return {"messages":[message],
-                "previewToken":null,
-                "codeModelMode":true,
-                "agentMode":useModel?{
-                  "mode":useModel?true:false,
-                  "id":model,
-                  "name":"GOD"}:{},
-                "trendingAgentMode":{},
-                "isMicMode":false,
-                "isChromeExt":false,
-                "githubToken":null,
-                "clickedAnswer2":false,
-                "clickedAnswer3":false,
-                "clickedForceWebSearch":false,
-                "visitFromDelta":null
-               }
-
-
+function buildResponse(message,useModel,model,prompt){
+  return {
+  "messages": [
+    message
+  ],
+  "previewToken": null,
+  "codeModelMode": true,
+  "agentMode": useModel?{
+    "mode":useModel?true:false,
+    "id":model,
+    "name":"GOD"}:{},
+  "trendingAgentMode": {},
+  "isMicMode": false,
+  "maxTokens": 1024,
+  "playgroundTopP": 0.9,
+  "playgroundTemperature": 0.5,
+  "isChromeExt": false,
+  "githubToken": null,
+  "clickedAnswer2": false,
+  "clickedAnswer3": false,
+  "clickedForceWebSearch": false,
+  "visitFromDelta": false,
+  "userSystemPrompt":	null,
+  "mobileClient": false,
+  "userSelectedModel": null
+  }
 }
 
 const generateAlphanumericString = (length) => {
@@ -40,15 +52,11 @@ function getMessage(content,role){
           "role":(role==undefined)?"user":role
          }
 }
-async function ask(text,model){
-    let response=buildResponse(getMessage(text),model?true:false,model)
-    let r=await fetch("https://blackbox.ai/api/chat",{
-method:"post",
-headers:{"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.3"},
-body:JSON.stringify(response)
-    });
-    let result=await r.text();
-    return result.split("$@$").slice(2).join('$@$')
+async function ask(text,model,prompt){
+    let response=buildResponse(getMessage(text),model?true:false,model,prompt?prompt:null)
+    let r=await axios.post("https://blackbox.ai/api/chat",response);
+    let result=r.data;
+    return result
 }
 async function chat(client,msg,args){
    let text=await ask(args.join(' '))
@@ -59,13 +67,22 @@ async function dan(client,msg,args){
     let text=await ask(args.join(' '),model)
     await msg.reply(text)
 }
+async function custom(client,msg,args){
+    let text=await ask(args.join(' '),model,customPrompt)
+    await msg.reply(text)
+}
 
 module.exports=[
      {name:'ia',
      func:chat,
-     info:'Habla con la IA',args:[{name:'mensaje',info:'Mensaje que enviarle'}]},
+     info:'Habla con la IA',args:[{name:'mensaje',info:'Mensaje que enviarle',required:true}]},
      {name:'dan',
      func:dan,
-     info:'Habla con la IA(Agresiva)',args:[{name:'mensaje',info:'Mensaje que enviarle'}]}
+     info:'Habla con la IA(Agresiva)',args:[{name:'mensaje',info:'Mensaje que enviarle',required:true}]},
+     {name:'iafing',
+      func:custom,
+      info:'IA Personalizada para estudiantes',
+      args:[{name:'mensaje',info:'Mensaje que enviarle',required:true}]
+    }
 ]
 
